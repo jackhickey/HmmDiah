d255b31dfae3f81c9ff3f7197a969503c3d6a9e4
jack <jackhickey@gmail.com>
2017-09-09 09:01:44 +0100

    Bootstrap with spring initializr

---
+++ src/main/kotlin/io/jackh/hmmdiah/HmmdiahApplication.kt
@@ -0,0 +1,11 @@
+package io.jackh.hmmdiah
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HmmdiahApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(HmmdiahApplication::class.java, *args)
+}
---
+++ src/test/kotlin/io/jackh/hmmdiah/HmmdiahApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.jackh.hmmdiah
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HmmdiahApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
